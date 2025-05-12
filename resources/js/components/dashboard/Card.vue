8eb359a5ca0e3d1a20a5a56d5e58d793bb49cbc1
arianitSadriu <122325048+arianitSadriu@users.noreply.github.com>
2025-05-12 19:25:15 +0200

    created homepage/landingpage

---
+++ resources/js/components/dashboard/Card.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-700 dark:border-gray-700 hover:scale-x-105 transition text-white">
+        <div class="self-start font-extrabold text-xl"><slot name="numri"></slot></div>
+        <div class="py-8 font-semibold italic text-center">
+            <h3><slot name="heading"></slot></h3>
+            <p><slot name="p"></slot></p>
+        </div>
+        <div><slot name="b"></slot></div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/dashboard/Footer.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <footer class="pt-14 pb-4 bg-opacity-50 backdrop-blur-lg bg-gray-500">
+        <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 text-sm mt-14 text-center md:text-left">
+
+            <!-- Logo Section -->
+            <div class="-mt-8 flex items-start justify-center md:col-span-1">
+                <img  :src="'storage/images/logo.png'" class="md:w-[180px] md:h-[180px] w-[130px] h-[130px]">
+            </div>
+
+            <div>
+                <h4 class="uppercase font-bold text-xs sm:text-sm mb-3 tracking-wide text-gray-100">CONTACT</h4>
+                <ul class="space-y-2 mb-16 sm:text-center md:text-left">
+                    <li>
+                        <a href="#" class="footer-link mb-2 text-center sm:text-left transition-transform duration-200 transform hover:scale-110 hover:translate-x-2 hover:text-white">
+                            <span class="arrow">></span> Email
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link mb-2 text-center sm:text-left transition-transform duration-200 transform hover:scale-110 hover:translate-x-2 hover:text-white">
+                            <span class="arrow">></span> Phone number
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link mb-2 text-center sm:text-left transition-transform duration-200 transform hover:scale-110 hover:translate-x-2 hover:text-white">
+                            <span class="arrow">></span> City
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- Social Media Section -->
+            <div>
+                <h4 class="uppercase font-bold text-xs sm:text-sm mb-3 tracking-wide text-gray-100">SOCIAL MEDIA</h4>
+                <ul class="space-y-2 mb-16 sm:text-center md:text-left">
+                    <li>
+                        <a href="https://www.facebook.com" class="footer-link mb-2 text-center sm:text-left transition-transform duration-200 transform hover:scale-110 hover:translate-x-2 hover:text-white">
+                            <span class="arrow">></span> Facebook
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com" class="footer-link mb-2 text-center sm:text-left transition-transform duration-200 transform hover:scale-110 hover:translate-x-2 hover:text-white">
+                            <span class="arrow">></span> Instagram
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com" class="footer-link mb-2 text-center sm:text-left transition-transform duration-200 transform hover:scale-110 hover:translate-x-2 hover:text-white">
+                            <span class="arrow">></span> LinkedIn
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- Learn More Section -->
+            <div>
+                <h4 class="uppercase font-bold text-xs sm:text-sm mb-3 tracking-wide text-gray-100">LEARN MORE</h4>
+                <ul class="space-y-2 mb-16 sm:text-center md:text-left">
+                    <li>
+                        <a href="#" class="footer-link mb-2 text-center sm:text-left transition-transform duration-200 transform hover:scale-110 hover:translate-x-2 hover:text-white">
+                            <span class="arrow">></span> Community
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link mb-2 text-center sm:text-left transition-transform duration-200 transform hover:scale-110 hover:translate-x-2 hover:text-white">
+                            <span class="arrow">></span> Privacy Policy
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link mb-2 text-center sm:text-left transition-transform duration-200 transform hover:scale-110 hover:translate-x-2 hover:text-white">
+                            <span class="arrow">></span> Terms Of Use
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- Footer Bottom -->
+        <div class="container mx-auto flex justify-center items-center w-full mb-3 text-xs border-t border-white-500 pt-8 footer-bottom">
+            <div class="text-center w-full">
+                <p>Copyright FindJob. . All Rights Reserved.</p>
+            </div>
+        </div>
+    </footer>
+</template>
